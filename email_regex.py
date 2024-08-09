bc8be2ee313af16b1d8e7c8fc6fc32ed04bdd8c1
TPlatts04 <thomasplatts2004@gmail.com>
2024-08-09 19:49:14 +0100

    Changed main code to better suit UX

--- email_regex.py
+++ email_regex.py
@@ -1,13 +1,16 @@
 import re
 
 def main():
+    print("Welcome to the is it a valid email checker!")
     user_email = input("Please enter your email: ")
     email_test_reg(user_email)
 
 
 def email_test_reg(user_email):
     if matches := bool(re.search(r"^[^\.][\w!#$%&'*+\-#\/=?^`{|}~]{1,64}@[\w\-\.]{0,255}(gmail|hotmail|outlook|yahoo)\.(com|net|org|co\.uk)$", user_email, flags=re.IGNORECASE | re.ASCII)):
-        return matches
+        print(f"{user_email} is a valid email!")
+    else:
+        print(f"{user_email} is an invalid email!")
 
 
 if __name__ == "__main__":
